4ccaf76bee508f3b3e3830b49fece587d90cef97
xiamubobby <xiamubobby@gmail.com>
2014-11-13 14:05:17 +0800

    intialed

---
+++ app/src/main/java/com/xiamubobby/mustick/MainActivity.java
@@ -0,0 +1,94 @@
+package com.xiamubobby.mustick;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+
+public class MainActivity extends Activity {
+
+    Bitmap b;
+    final int REQUEST_CODE = 1;
+    ImageView imgv;
+    TextView txtv;
+    EditText edtx;
+    MuStickPrev mstc;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imgv = (ImageView) findViewById(R.id.canvas);
+        txtv = (TextView) findViewById(R.id.title);
+        edtx = (EditText) findViewById(R.id.title_inp);
+        mstc = (MuStickPrev) findViewById(R.id.mup);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void picSelButClicked(View v) {
+        Intent bgImgIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        bgImgIntent.setType("image/*"); // intent type to filter application based on your requirement
+        startActivityForResult(bgImgIntent, REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK)  {
+            if (b!=null) {
+                b.recycle();
+            }
+            InputStream stream = null;
+            try {
+                stream = getContentResolver().openInputStream(data.getData());
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            b = BitmapFactory.decodeStream(stream);
+            //imgv.setImageBitmap(b);
+            mstc.setImg(b);
+            try {
+                stream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    public void titleRef(View v) {
+        txtv.setText(edtx.getText());
+    }
+}
---
+++ app/src/main/java/com/xiamubobby/mustick/MuStickPrev.java
@@ -0,0 +1,56 @@
+package com.xiamubobby.mustick;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2014/11/13.
+ */
+public class MuStickPrev extends View {
+
+    Bitmap b;
+    String t;
+    int bgc;
+
+    public MuStickPrev(Context context) {
+        super(context);
+    }
+
+    public MuStickPrev(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MuStickPrev(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public void setImg(Bitmap barg) {
+        b = barg;
+        invalidate();
+    }
+    public void setTitle(String targ) {
+        t = targ;
+        invalidate();
+    }
+    public void setBgColor(int carg) {
+        bgc = carg;
+        invalidate();
+    }
+
+    @Override
+    public void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint p = new Paint();
+        if (b != null) {
+            canvas.drawBitmap(b, null, new RectF(0, 0, getWidth(), getHeight()), p);
+        }
+    }
+
+
+}
